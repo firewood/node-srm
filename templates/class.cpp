0c3d439f2b112c8de509eff5544d233ce48fddb8
firewood <karamaki@gmail.com>
2012-03-07 01:29:43 +0900

    C++ code template

---
+++ templates/class.cpp
@@ -0,0 +1,33 @@
+#undef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> IntVec;
+typedef vector<string> StrVec;
+
+class $CLASSNAME$ {
+
+public:
+	$RC$ $METHODNAME$($METHODPARMS$) {
+		$RC$ result;
+
+
+		return result;
+	}
+
+	$WRITERCODE$
+
+	$TESTCODE$
+};
