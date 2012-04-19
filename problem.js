a825daf57ade2fd2d9529460f60454a518acaf28
firewood <karamaki@gmail.com>
2012-04-19 18:20:02 +0900

    array handling in C++

--- problem.js
+++ problem.js
@@ -23,28 +23,73 @@ function td(tr) {
 	return a;
 }
 
+function array_to_vector(s) {
+	return s.replace(/(\w+)\[\]/g, function(m, tag) {
+		return 'vector<' + tag + '>';
+	});
+}
+
 function statement_to_code(path, statement, callback) {
+	var r = {'Class:':'CLASSNAME', 'Method:':'METHODNAME', 'Parameters:':'METHODPARMS', 'Returns:':'RC', 'Method signature:':'METHODSIGNATURE'};
 	var params = {};
 	statement.split('<h3>').map(function(element) {
 		if (element.match(/^Definition/)) {
 			element.split('<tr>').map(function(element) {
 				var a = td(element);
 				if (a.length >= 2) {
-					params[a[0]] = a[1];
+					var key = a[0];
+					if (r.hasOwnProperty(key)) {
+						key = r[key];
+					}
+					params[key] = a[1];
 				}
 			});
 		}
 	});
+	if (!params.hasOwnProperty('CLASSNAME')) {
+		callback('No content');
+		return;
+	}
+
+	var argtypes = params['METHODPARMS'].split(',');
+	cout("method args", argtypes);
+	var argvars = [];
+
+	var i;
+	var test_code = '// BEGIN CUT HERE\n';
+	test_code += '	void run_test(string s) {\n';
+	test_code += '		int pos = 0;\n';
+	for (i = 0; i < argtypes.length; ++i) {
+		var a = 'a' + i;
+		var t = argtypes[i].trim();
+		if (t.match(/(\w+)\[\]/)) {
+			t = array_to_vector(t);
+			test_code += '		' + t + ' ' + a + ' = parse_array<' + RegExp.$1 + '>(s, pos);\n';
+		} else {
+			test_code += '		' + t + ' ' + a + ' = parse<' + t + '>(s, pos);\n';
+		}
+		argvars.push(a);
+	}
+	test_code += '		$RC$ res = $METHODNAME$(' + argvars.join(', ') + ');\n';
+	test_code += '		cout << res << endl;\n';
+	test_code += '	}\n';
+	test_code += '// END CUT HERE\n';
+
+	if (params.hasOwnProperty('METHODSIGNATURE')) {
+		var args = params['METHODSIGNATURE'].replace(params['RC'] + ' ' + params['METHODNAME'], '');
+		args = args.trim();
+		args = args.substr(1, args.length - 2);
+		args = array_to_vector(args);
+		params['METHODPARMS'] = args;
+	}
+	params['RC'] = array_to_vector(params['RC']);
 
 	fs.readFile('templates/class' + ext(), 'utf8', function(err, content) {
 		if (!err) {
-			var r = {CLASSNAME: 'Class:', METHODNAME: 'Method:', METHODPARMS: 'Parameters:',
-				RC: 'Returns:'};
-			content = content.replace(/\x24([A-Z]+)\x24/g, function(m, tag) {
-				if (r.hasOwnProperty(tag)) {
-					if (params.hasOwnProperty(r[tag])) {
-						return params[r[tag]];
-					}
+			content = content.replace("$TESTCODE$", test_code)
+					.replace(/\x24([A-Z]+)\x24/g, function(m, tag) {
+				if (params.hasOwnProperty(tag)) {
+					return params[tag];
 				}
 				return '';
 			});
